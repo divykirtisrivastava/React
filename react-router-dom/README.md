e643ae06dd9295935e21780ec4208202f30baa08
divykirtisivastava <divykirtisivastava@gmail.com>
2024-04-23 19:42:04 -0700

    add all files

---
+++ react-router-dom/README.md
@@ -0,0 +1,17 @@
+# Project Title ⭐
+
+This folder contains the code that how to to setup react-router-dom for single page web application
+
+
+### Prerequisites
+
+Yoy need to knw about how to setup tailwind with react template
+
+### Installation ✨
+
+```JAVASCRIPT
+npm install react-router-dom
+npm run dev
+
+
+
---
+++ react-router-dom/src/main.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import Layout from './Layout'
+import Home from './components/Home'
+import About from './components/About'
+import Contact from './components/Contact'
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout/>}>
+      <Route path='' element={<Home/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/contact' element={<Contact/>}/>
+    </Route>
+  )
+)
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
